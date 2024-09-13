a73ed1cd9c7fd0a82841a5dbababccff8d676233
Hector Colon <hector@hec.to>
2024-09-12 23:23:25 -0400

    cleanup repo and add readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,23 @@
-five
+## five
+
+Insipred by this youtube video:
+https://www.youtube.com/watch?v=_-AfhLQfb6w
+
+Using only 5-letter words, can you find 5 words where together they don't have any letters repeated?
+
+There are 26 letters, so for every answer you will be using every letter except for one.
+
+It turns out there are 10 sets of five five-letter words! The output of this program is:
+```solution found: waqfs, bling, treck, vozhd, jumpy,
+solution found: waqfs, brick, vozhd, glent, jumpy,
+solution found: waqfs, blunk, cimex, vozhd, grypt,
+solution found: waqfs, brung, cylix, vozhd, kempt,
+solution found: waqfs, bemix, clunk, vozhd, grypt,
+solution found: waqfs, jumby, prick, vozhd, glent,
+solution found: waqfs, jumby, clipt, vozhd, kreng,
+solution found: waqfs, jumby, treck, vozhd, pling,
+solution found: waltz, vibex, chunk, fjord, gymps,
+solution found: waltz, vibex, gucks, fjord, nymph,
+```
+
+Runs in about 20 seconds on my desktop computer.
--- five.cpp
+++ five.cpp
@@ -9,138 +9,142 @@ void find_five_ll(uint32_t l);
 
 struct word
 {
-	std::string s;
-	uint32_t n = 0;
+    std::string s;  //ascii representation of word
+    uint32_t n = 0; //numerical (binary) representation of word
 
-	bool operator<(const word& other)  const { return n < other.n; }
+    bool operator<(const word& other)  const { return n < other.n; }
 
-	bool operator==(const word& other)  const { return n == other.n; }
+    bool operator==(const word& other)  const { return n == other.n; }
 };
 
 struct answer
 {
-	std::vector<struct word> w;
-	uint32_t n = 0;
+    std::vector<struct word> w; //an answer will consist of 5 words
+    uint32_t n = 0; // binary representation of all the letters used in the answer
 };
 
+// the answer we are currently building
 struct answer current_answers;
 
+// group all words into one of 26 sets, depending on the 'lowest' letter they contain
 std::array<std::set<struct word>, 26> letter_list;
 
+//string-to-number: convert the ascii representation to binary.
+//lowest bit represents an 'a', second lowest is 'b', and so on.
 void ston(std::string s)
 {
-	struct word w;
-	uint32_t n = 0;
+    struct word w;
+    uint32_t n = 0;
 
-	for (int i = 0; i < 5; i++)
-	{
-		uint32_t bit = s.at(i) - 'a';
+    for (int i = 0; i < 5; i++)
+    {
+        uint32_t bit = s.at(i) - 'a';
 
-		//check for duplicate letters
-		if (((n >> bit) & (uint32_t)1) == 1)
-		{
-			return;
-		}
+        //check for duplicate letters
+        if (((n >> bit) & (uint32_t)1) == 1)
+        {
+            return;
+        }
 
-		n |= (1 << bit);
-	}
+        n |= (1 << bit);
+    }
 
-	w.n = n;
-	w.s = s;
-	auto i = 0;
-	while ((n & (uint32_t)1) == 0)
-	{
-		n >>= 1;
-		i++;
-	}
-	letter_list[i].insert(w);
+    w.n = n;
+    w.s = s;
+    auto i = 0;
+    while ((n & (uint32_t)1) == 0)
+    {
+        n >>= 1;
+        i++;
+    }
+    letter_list[i].insert(w);
 }
 
 
 int main()
 {
-	//std::vector<struct word> words;
-	std::set<uint32_t> anagram_checker;
-	std::string line;
+    std::set<uint32_t> anagram_checker;
+    std::string line;
 
-	time_t start;
-	time(&start);
+    time_t start;
+    time(&start);
 
-	printf("hello, searching for 5 5-letter words with no letters in common!\n");
+    printf("hello, searching for 5 5-letter words with no letters in common!\n");
 
-	std::ifstream allowed_words("wordle-allowed-guesses.txt");
-	while (std::getline(allowed_words, line))
-	{
-		(void)ston(line);
-	}
-	printf("loaded allowed wordle guesses\n");
+    std::ifstream allowed_words("wordle-allowed-guesses.txt");
+    while (std::getline(allowed_words, line))
+    {
+        (void)ston(line);
+    }
+    printf("loaded allowed wordle guesses\n");
 
-	std::ifstream answer_words("wordle-answers-alphabetical.txt");
-	while (std::getline(answer_words, line))
-	{
-		(void)ston(line);
-	}
-	printf("loaded wordle answers\n");
+    std::ifstream answer_words("wordle-answers-alphabetical.txt");
+    while (std::getline(answer_words, line))
+    {
+        (void)ston(line);
+    }
+    printf("loaded wordle answers\n");
 
-	// for every letter_list, try to build an answer list
-	for (uint32_t i = 0; i < letter_list.size(); i++)
-	{
-		for(auto &current_word : letter_list[i])
-		{
-			current_answers.w.clear();
-			current_answers.n = 0;
+    // for every letter_list, try to build an answer list
+    for (uint32_t i = 0; i < letter_list.size(); i++)
+    {
+        for(auto &current_word : letter_list[i])
+        {
+            current_answers.w.clear();
+            current_answers.n = 0;
 
-			current_answers.w.push_back(current_word);
-			current_answers.n = current_word.n;
-			find_five_ll(i);
-		}
-	}
+            current_answers.w.push_back(current_word);
+            current_answers.n = current_word.n;
+            find_five_ll(i);
+        }
+    }
 
-	time_t end;
-	time(&end);
-	printf("finished in %f seconds\n", difftime(end, start));
-	return 0;
+    time_t end;
+    time(&end);
+    printf("finished in %f seconds\n", difftime(end, start));
+    return 0;
 }
 
 
 void find_five_ll(uint32_t l)
 {
-	if (current_answers.w.size() == 5)
-	{
-		printf("solution found: ");
-		for (auto answer : current_answers.w)
-		{
-			printf("%s, ", answer.s.c_str());
-		}
-		printf("\n");
-	}
-	else
-	{
-		// search for the next letter we need
-		uint32_t i = l + 1;
-		uint32_t letters = current_answers.n >> i;
+    if (current_answers.w.size() == 5)
+    {
+        printf("solution found: ");
+        for (auto answer : current_answers.w)
+        {
+            printf("%s, ", answer.s.c_str());
+        }
+        printf("\n");
+    }
+    else
+    {
+        // search for the next letter we need.
+        // i keeps track of the 'index' of the letter we're checking
+        // letters keeps track of the letters were searching for: keep shifting right until we hit a zero
+        uint32_t i = l + 1;
+        uint32_t letters = current_answers.n >> i;
 
-		
-		while ((letters & (uint32_t)1) == 1)
-		{
-			letters >>= 1;
-			i++;
-		}
+        while ((letters & (uint32_t)1) == 1)
+        {
+            letters >>= 1;
+            i++;
+        }
 
-		// found a missing letter
-		for (auto next : letter_list[i])
-		{
-			if ((current_answers.n & next.n) == 0)
-			{
-				// we found a new word to add
-				current_answers.n |= next.n;
-				current_answers.w.push_back(next);
-				find_five_ll(i);
-			}
-		}
-	}
+        // found a missing letter
+        for (auto next : letter_list[i])
+        {
+            if ((current_answers.n & next.n) == 0)
+            {
+                // we found a new word to add
+                current_answers.n |= next.n;
+                current_answers.w.push_back(next);
+                find_five_ll(i);
+            }
+        }
+    }
 
-	// current word is a dead-end, remove it from the current_answers
-	current_answers.n ^= current_answers.w.back().n;
-	current_answers.w.pop_back();
+    // current word is a dead-end, remove it from the current_answers
+    current_answers.n ^= current_answers.w.back().n;
+    current_answers.w.pop_back();
 }
